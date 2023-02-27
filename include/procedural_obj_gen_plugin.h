469cecd855c6a65e763ec1d1f2898b9b85618d43
matteocaruso1993 <matteo.caruso@phd.units.it>
2023-02-27 11:25:37 +0100

    Introduction terrain model plugin

---
+++ include/procedural_obj_gen_plugin.h
@@ -0,0 +1,44 @@
+#include <string>
+#include <gazebo/gazebo.hh>
+#include <sdf/sdf.hh>
+#include <gazebo/sensors/sensors.hh>
+#include <gazebo/gazebo_client.hh>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/common/common.hh>
+#include <gazebo/common/Plugin.hh>
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+
+
+
+namespace gazebo
+{
+    class proceduralObjGen : public ModelPlugin
+    {
+        public:
+            proceduralObjGen(); // Constructor
+
+            ~proceduralObjGen(); // Destructor
+
+            void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
+
+            virtual void OnUpdate(void);
+
+            bool spawnObj(double x, double y);
+
+        private:
+            bool getPolygon(double x, double y);
+
+            physics::ModelPtr model;
+
+            physics::ModelPtr terrainModel;
+
+            sdf::ElementPtr sdf;
+
+            physics::WorldPtr world;
+
+            physics::LinkPtr linkTerrain;
+    };
+    GZ_REGISTER_MODEL_PLUGIN(proceduralObjGen);
+}
---
+++ src/procedural_obj_gen_plugin.cpp
@@ -0,0 +1,71 @@
+#include "../include/procedural_obj_gen_plugin.h"
+
+using namespace gazebo;
+
+// Constructor
+proceduralObjGen::proceduralObjGen()
+{
+    
+}
+
+// Destructor
+proceduralObjGen::~proceduralObjGen()
+{
+    
+}
+
+void proceduralObjGen::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+{
+    // Loading plugin
+    this -> model = _parent;
+    this -> sdf = _sdf;
+    this -> world = this -> model -> GetWorld(); // Get pointer to the simulation world
+
+    this -> terrainModel = this -> world -> ModelByName("DEM_100x20_v1"); //Get pointer to the terrain model
+    this -> linkTerrain = this -> terrainModel -> GetLink("link_0"); //Get pointer to the terrain link
+    physics::CollisionPtr collision_terrain = this -> linkTerrain -> GetCollision(0);
+}
+
+void proceduralObjGen::OnUpdate(void)
+{
+
+}
+
+bool proceduralObjGen::spawnObj(double x, double y)
+{
+
+}
+
+bool proceduralObjGen::getPolygon(double x, double y)
+{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
