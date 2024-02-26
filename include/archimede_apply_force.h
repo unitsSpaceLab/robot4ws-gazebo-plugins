eb3ff94f484dc69a2787156d041375157859be9b
SimoneCottiga <smcotti@gmail.com>
2024-02-26 11:29:24 +0100

    Fix typo

--- include/archimede_apply_force.h
+++ include/archimede_apply_force.h
@@ -66,7 +66,7 @@ namespace gazebo
 
             transport::NodePtr node_pt; //Node for gazebo communication
 
-            event::ConnectionPtr onnection;
+            event::ConnectionPtr connection;
 
             //Publisher
             transport::PublisherPtr pub;
